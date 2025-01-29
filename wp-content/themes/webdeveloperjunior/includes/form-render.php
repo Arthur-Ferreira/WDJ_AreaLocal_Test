63fb07b5dc347f219634ba5535a8493507f83ec9
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-01-28 21:30:59 -0300

    Indentation fix at form-render

--- wp-content/themes/webdeveloperjunior/includes/form-render.php
+++ wp-content/themes/webdeveloperjunior/includes/form-render.php
@@ -1,21 +1,21 @@
 <?php
 
 function form_render() {
-  ?>
-    <section>
-      <form id="form" method="post" name="postform" enctype="multipart/form-data" action="">
-        <label for="title">Título: </label>
-        <input type="text" id="title" name="title" required>
+ ?>
+  <section>
+    <form id="form" method="post" name="postform" enctype="multipart/form-data" action="">
+      <label for="title">Título: </label>
+      <input type="text" id="title" name="title" required>
 
-        <label for="content">Conteúdo: </label>
-        <textarea id="content" name="content" required></textarea>
+      <label for="content">Conteúdo: </label>
+      <textarea id="content" name="content" required></textarea>
 
-        <label for="image">Imagem Destacada: </label>
-        <input type="file" id="image" name="image" required>
+      <label for="image">Imagem Destacada: </label>
+      <input type="file" id="image" name="image" required>
 
-        <button type="submit" name="submit-post">Enviar</button>
-      </form>
-    </section>
-    <?php
+      <button type="submit" name="submit-post">Enviar</button>
+    </form>
+  </section>
+  <?php
 }
 ?>
