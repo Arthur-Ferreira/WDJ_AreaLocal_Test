57e7d5add322c9706efc5a94aec8d33c99401220
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-01-28 21:28:58 -0300

    Indentation fix at pagination

--- wp-content/themes/webdeveloperjunior/includes/pagination.php
+++ wp-content/themes/webdeveloperjunior/includes/pagination.php
@@ -3,10 +3,10 @@
 function pagination($query) {
   $big = 999999999;
   echo paginate_links([
-    'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-    'format' => '?paged=%#%',
-    'current' => max(1, get_query_var('paged')),
-    'total' => $query->max_num_pages,
+    'base'      => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+    'format'    => '?paged=%#%',
+    'current'   => max(1, get_query_var('paged')),
+    'total'     => $query->max_num_pages,
     'prev_text' => '«',
     'next_text' => '»',
   ]);
