7f1de46bd59a3163baa0e8b820000178d5754a9e
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-01 14:42:08 -0300

    Create header and footer components

---
+++ wp-content/themes/webdeveloperjunior/footer.php
@@ -0,0 +1,11 @@
+<footer>
+  <div class="footer-container">
+    <p>
+      Made with 🖤 by ARTHUR FERREIRA
+    </p>
+  </div>
+</footer>
+
+</body>
+
+</html>
---
+++ wp-content/themes/webdeveloperjunior/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title('|', true, 'right'); ?></title>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+
+  <body <?php body_class(); ?>>
+
+    <header>
+      <div class="header-container">
+        <div class="logo-wrapper">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arealocal-logo.png" alt="Logo da empresa">
+        </div>
+      </div>
+
+    </header>
