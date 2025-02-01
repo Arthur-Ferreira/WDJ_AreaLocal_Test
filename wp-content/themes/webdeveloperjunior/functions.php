cf1ed16b1fb8abcea0c423c882f475c5a55f0537
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-01 02:41:56 -0300

    Bug fix at styles enqueue and add script enqueue funcion

--- wp-content/themes/webdeveloperjunior/functions.php
+++ wp-content/themes/webdeveloperjunior/functions.php
@@ -1,14 +1,24 @@
 <?php
 
 function child_theme_enqueue_styles() {
-  wp_enqueue_style('parent-style', get_template_directory_uri() . '/assets/css/style.css');
+  wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css');
 }
 add_action('wp_enqueue_scripts', 'child_theme_enqueue_styles');
 
-add_action( 'after_setup_theme', 'custom_post_format_setup' );
+
+function child_theme_enqueue_scripts() {
+  wp_enqueue_script('form-ajax', get_template_directory_uri() . '/assets/js/form-ajax.js', array('jquery'), null, true);
+
+  wp_localize_script('form-ajax', 'ajax_object', array(
+    'ajax_url' => admin_url('admin-ajax.php') // WordPress Ajax URL
+  ));
+}
+add_action('wp_enqueue_scripts', 'child_theme_enqueue_scripts');
+
 function custom_post_format_setup() {
   add_theme_support( 'post-formats', array( 'aside', 'gallery', 'quote' ) );
 }
+add_action( 'after_setup_theme', 'custom_post_format_setup' );
 
 require_once get_template_directory() . '/includes/form-render.php';
 require_once get_template_directory() . '/includes/form-handler.php';
@@ -16,11 +26,3 @@ require_once get_template_directory() . '/includes/pagination.php';
 require_once get_template_directory() . '/templates/posts-list.php';
 require_once get_template_directory() . '/templates/home.php';
 
-// **Configurações do tema**
-function theme_setup() {
-  // Suporte a miniaturas (imagens destacadas)
-  add_theme_support('post-thumbnails');
-  // Suporte ao título dinâmico
-  add_theme_support('title-tag');
-}
-add_action('after_setup_theme', 'theme_setup');
