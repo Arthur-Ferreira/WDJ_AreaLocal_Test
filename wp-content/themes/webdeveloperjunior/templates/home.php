601b982d5202d9cff203335c83d01370acf4102a
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-01 15:42:30 -0300

    Add HTML elements and containers to improve SEO

--- wp-content/themes/webdeveloperjunior/templates/home.php
+++ wp-content/themes/webdeveloperjunior/templates/home.php
@@ -1,14 +1,25 @@
 <?php get_header(); ?>
 
 <main>
-  <h1>Bem-vindo ao Wordpress Posts</h1>
-    <?php 
-    // Exibe o formulário de postagem
-    form_render();
-    
-    // Exibe a lista de postagem
-    render_posts();
-    ?>
+
+  <section class="wrapper">
+    <div class="container">
+      <div class="info">
+        <span>
+          curtiu a Área Local?
+        </span>
+        <h1>Bem-vindo ao Wordpress Posts</h1>
+      </div>
+      <?php
+      // Exibe o formulário de postagem
+      form_render();
+      ?>
+    </div>
+  </section>
+  <?php
+  // Exibe a lista de postagem
+  render_posts();
+  ?>
 </main>
 
 <?php get_footer(); ?>
