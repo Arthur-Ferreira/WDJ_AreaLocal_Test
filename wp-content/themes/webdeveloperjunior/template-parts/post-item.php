5b731446fd56c86ae8a2b940e85d2bb0d3815061
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-01 02:39:44 -0300

    Add classes and id to style and for js interaction

--- wp-content/themes/webdeveloperjunior/template-parts/post-item.php
+++ wp-content/themes/webdeveloperjunior/template-parts/post-item.php
@@ -1,5 +1,8 @@
 <article>
-  <h2><?php the_title(); ?></h2>
+  <!-- <?php if ($image) : ?> -->
+    <!-- <img src="<?php echo $image; ?>" alt="<?php the_title(); ?>"> -->
+  <!-- <?php endif; ?> -->
+  <h3><?php the_title(); ?></h3>
   <p><?php the_excerpt(); ?></p>
   <a href="<?php the_permalink(); ?>">Saiba mais</a>
 </article>
