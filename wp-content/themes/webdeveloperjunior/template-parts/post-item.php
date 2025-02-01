776f7f3fb8f43aa0987f474f63f71ac46677f483
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-01 15:41:39 -0300

    Add containers and classes to improve post article apperence

--- wp-content/themes/webdeveloperjunior/template-parts/post-item.php
+++ wp-content/themes/webdeveloperjunior/template-parts/post-item.php
@@ -1,11 +1,15 @@
-<article>
+<article class="post">
   <?php
   $image = get_the_post_thumbnail_url(get_the_ID()); // Obtém a imagem destacada
   if ($image) :
   ?>
-    <img src="<?php echo esc_url($image); ?>" alt="<?php the_title(); ?>">
+    <div class="post-image">
+      <img src="<?php echo esc_url($image); ?>" alt="<?php the_title(); ?>">
+    </div>
   <?php endif; ?>
-  <h3><?php the_title(); ?></h3>
-  <p><?php the_excerpt(); ?></p>
-  <a href="<?php the_permalink(); ?>">Saiba mais</a>
+  <div class="post-info">
+    <h3><?php the_title(); ?></h3>
+    <p><?php the_excerpt(); ?></p>
+    <a href="<?php the_permalink(); ?>">Saiba mais</a>
+  </div>
 </article>
