a39c9356962558660979edcbaae196c34fa5cbb2
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-01 02:40:41 -0300

    Remove post-item from this file to improve maintenance

--- wp-content/themes/webdeveloperjunior/templates/posts-list.php
+++ wp-content/themes/webdeveloperjunior/templates/posts-list.php
@@ -1,25 +1,34 @@
 <?php
 
-function render_posts() {
-  $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-  $query = new WP_Query( [
+function render_posts()
+{
+  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+  $query = new WP_Query([
     'post_type'        => 'post',
     'posts_per_page'   => 9,
     'paged'            => $paged
   ]);
 
-  if($query -> have_posts()) {
-    ?>
+  if ($query->have_posts()) {
+?>
     <section>
-      <?php
-        while($query -> have_posts()) {
-          $query -> the_post();
-          get_template_part('template-parts/post-item', 'post');
+      <ul id="posts-list" class="posts-list">
+        <?php
+        while ($query->have_posts()) {
+        ?>
+          <li class="post-item">
+            <?php
+            $query->the_post();
+            get_template_part('template-parts/post-item', 'post');
+            ?>
+          </li>
+        <?php
         }
-        pagination($query);
-      ?>
+        ?>
+      </ul>
     </section>
     <?php
+    pagination($query);
   } else {
     echo 'Nenhum post encontrado';
   }
