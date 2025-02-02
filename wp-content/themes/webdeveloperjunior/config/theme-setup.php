15353d61d8f98d64439744409b0a249bc3176c18
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-02 01:13:41 -0300

    Bug fix at footer missing wp_footer() function and header remove unnecessary body tag

--- wp-content/themes/webdeveloperjunior/config/theme-setup.php
+++ wp-content/themes/webdeveloperjunior/config/theme-setup.php
@@ -3,7 +3,7 @@
 function webdeveloperjunior_theme_setup()
 {
   // Suporte a thumbnails
-  add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+  add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'webdeveloperjunior_theme_setup');
--- wp-content/themes/webdeveloperjunior/header.php
+++ wp-content/themes/webdeveloperjunior/header.php
@@ -8,15 +8,13 @@
   <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
-  <body <?php body_class(); ?>>
-
-    <header>
-      <div class="header-container">
-        <div class="logo-wrapper">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arealocal-logo.png" alt="Logo da empresa">
-        </div>
+  <header>
+    <div class="header-container">
+      <div class="logo-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arealocal-logo.png" alt="Logo da empresa">
       </div>
+    </div>
 
-    </header>
+  </header>
