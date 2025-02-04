26f8c04c2277c86312260d628b7a092e818b8066
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-04 00:17:29 -0300

    Add forms error span and content

--- wp-content/themes/webdeveloperjunior/includes/form-render.php
+++ wp-content/themes/webdeveloperjunior/includes/form-render.php
@@ -5,15 +5,18 @@ function form_render()
 ?>
   <form id="post-form" method="post" name="postform" enctype="multipart/form-data">
     <label for="title">Título: </label>
-    <input type="text" id="title" name="title" required>
+    <input type="text" id="title" name="title">
+    <span id="title-error" class="error">Título não pode ser um campo vazio</span>
 
     <label for="content">Conteúdo: </label>
-    <textarea id="content" name="content" required></textarea>
+    <textarea id="content" name="content"></textarea>
+    <span id="content-error" class="error">Conteúdo não pode ser um campo vazio</span>
 
     <label for="image">Imagem Destacada: </label>
-    <input type="file" id="image" name="image" required>
+    <input type="file" id="image" name="image">
+    <span id="image-error" class="error">Imagem Destacada não pode ser um campo vazio</span>
 
-    <button id="submit-btn" type="submit">Enviar</button>
+    <button id="submit-btn">Enviar</button>
   </form>
 <?php
 }
