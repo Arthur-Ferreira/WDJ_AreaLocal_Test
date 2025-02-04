500746dc3fd5da94b0ed94e84e93c456d812439a
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-04 00:16:54 -0300

    Add forms validation logic

--- wp-content/themes/webdeveloperjunior/assets/js/form-ajax.js
+++ wp-content/themes/webdeveloperjunior/assets/js/form-ajax.js
@@ -1,11 +1,39 @@
+// Validações dos campos do formulário
+
+function fieldsValidation(fields) {
+  let isValid = true;
+
+  fields.forEach(function ($field) {
+    let fieldId = $field.attr('id');
+    let errorSpanId = `#${fieldId}-error`;
+
+    if ($field.val().trim() === '') {
+      jQuery(errorSpanId).addClass('show');
+      isValid = false;
+    } else {
+      jQuery(errorSpanId).removeClass('show');
+    }
+  });
+
+  return isValid;
+}
+
 jQuery(document).ready(function ($) {
   $('#submit-btn').on('click', function (e) {
     e.preventDefault(); // Evita o reload da página
 
+    // Seleciona os campos obrigatórios
+    var fields = [$('#title'), $('#content'), $('#image')];
+
+    // Valida os campos
+    if (!fieldsValidation(fields)) {
+      return;
+    }
+
     // Desbilitando o botão após o envio e muda o texto
     $('#submit-btn').prop('disabled', true).text('Enviando...');
-    
-    
+
+
     var form = $('#post-form')[0];
     var formData = new FormData(form);
     formData.append('action', 'submit_post'); // Adiciona a action post_form
@@ -20,7 +48,7 @@ jQuery(document).ready(function ($) {
         alert('Post enviado com sucesso!');
         form.reset();
         $('#posts-list').prepend(response.data); // Atualiza a lista de posts dinamicamente
-        
+        console.log(response)
         // Reabilitando o botão após o sucesso e reseta o texto
         $('#submit-btn').prop('disabled', false).text('Enviar');
       },
