c19a44c8ae261531aaae9626506988d3894a2ab7
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-03 22:14:01 -0300

    Add button disabled class to give the user a visual message that the from was sent

--- wp-content/themes/webdeveloperjunior/assets/js/form-ajax.js
+++ wp-content/themes/webdeveloperjunior/assets/js/form-ajax.js
@@ -1,8 +1,13 @@
 jQuery(document).ready(function ($) {
-  $('#post-form').on('submit', function (e) {
+  $('#submit-btn').on('click', function (e) {
     e.preventDefault(); // Evita o reload da página
 
-    var formData = new FormData(this);
+    // Desbilitando o botão após o envio e muda o texto
+    $('#submit-btn').prop('disabled', true).text('Enviando...');
+    
+    
+    var form = $('#post-form')[0];
+    var formData = new FormData(form);
     formData.append('action', 'submit_post'); // Adiciona a action post_form
 
     $.ajax({
@@ -13,11 +18,16 @@ jQuery(document).ready(function ($) {
       contentType: false,
       success: function (response) {
         alert('Post enviado com sucesso!');
-        $('#post-form')[0].reset();
+        form.reset();
         $('#posts-list').prepend(response.data); // Atualiza a lista de posts dinamicamente
+        
+        // Reabilitando o botão após o sucesso e reseta o texto
+        $('#submit-btn').prop('disabled', false).text('Enviar');
       },
       error: function () {
         alert('Erro ao enviar post.');
+        // Reabilitando o botão após o sucesso e reseta o texto
+        $('#submit-btn').prop('disabled', false).text('Enviar');
       }
     });
   });
